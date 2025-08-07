a06211cb40ba12991eef6bec0ef41788acfce3b8
romakholodetskiy <romakholodetskiy@gmail.com>
2025-08-07 23:07:46 +0300

    unset woocommerce fonts

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -23,52 +23,52 @@ function f1devsesl_woocommerce_setup() {
 }
 add_action( 'after_setup_theme', 'f1devsesl_woocommerce_setup' );
 
-/**
- * WooCommerce specific scripts & stylesheets.
- *
- * @return void
- */
-function f1devsesl_woocommerce_scripts() {
-	wp_enqueue_style( 'f1devs-esl-woocommerce-style', get_template_directory_uri() . '/assets/css/woocommerce.css' );
-
-	$font_path   = WC()->plugin_url() . '/assets/fonts/';
-	$inline_font = '@font-face {
-			font-family: "star";
-			src: url("' . $font_path . 'star.eot");
-			src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
-				url("' . $font_path . 'star.woff") format("woff"),
-				url("' . $font_path . 'star.ttf") format("truetype"),
-				url("' . $font_path . 'star.svg#star") format("svg");
-			font-weight: normal;
-			font-style: normal;
-		}';
-
-	wp_add_inline_style( 'f1devs-esl-woocommerce-style', $inline_font );
-}
-add_action( 'wp_enqueue_scripts', 'f1devsesl_woocommerce_scripts' );
-
-/**
- * Disable the default WooCommerce stylesheet.
- *
- * Removing the default WooCommerce stylesheet and enqueing your own will
- * protect you during WooCommerce core updates.
- *
- * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
- */
-add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
-
-/**
- * Add 'woocommerce-active' class to the body tag.
- *
- * @param  array $classes CSS classes applied to the body tag.
- * @return array $classes modified to include 'woocommerce-active' class.
- */
-function f1devsesl_woocommerce_active_body_class( $classes ) {
-	$classes[] = 'woocommerce-active';
-
-	return $classes;
-}
-add_filter( 'body_class', 'f1devsesl_woocommerce_active_body_class' );
+///**
+// * WooCommerce specific scripts & stylesheets.
+// *
+// * @return void
+// */
+//function f1devsesl_woocommerce_scripts() {
+//	wp_enqueue_style( 'f1devs-esl-woocommerce-style', get_template_directory_uri() . '/assets/css/woocommerce.css' );
+//
+//	$font_path   = WC()->plugin_url() . '/assets/fonts/';
+//	$inline_font = '@font-face {
+//			font-family: "star";
+//			src: url("' . $font_path . 'star.eot");
+//			src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
+//				url("' . $font_path . 'star.woff") format("woff"),
+//				url("' . $font_path . 'star.ttf") format("truetype"),
+//				url("' . $font_path . 'star.svg#star") format("svg");
+//			font-weight: normal;
+//			font-style: normal;
+//		}';
+//
+//	wp_add_inline_style( 'f1devs-esl-woocommerce-style', $inline_font );
+//}
+//add_action( 'wp_enqueue_scripts', 'f1devsesl_woocommerce_scripts' );
+//
+///**
+// * Disable the default WooCommerce stylesheet.
+// *
+// * Removing the default WooCommerce stylesheet and enqueing your own will
+// * protect you during WooCommerce core updates.
+// *
+// * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
+// */
+//add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+//
+///**
+// * Add 'woocommerce-active' class to the body tag.
+// *
+// * @param  array $classes CSS classes applied to the body tag.
+// * @return array $classes modified to include 'woocommerce-active' class.
+// */
+//function f1devsesl_woocommerce_active_body_class( $classes ) {
+//	$classes[] = 'woocommerce-active';
+//
+//	return $classes;
+//}
+//add_filter( 'body_class', 'f1devsesl_woocommerce_active_body_class' );
 
 /**
  * Products per page.
