9050fad7a1bcf773471500b39c8e1156f07137ab
totoseriapov <totoseriapov@gmail.com>
2025-08-11 16:46:07 +0300

    after trubbles

--- woocommerce/myaccount/dashboard.php
+++ woocommerce/myaccount/dashboard.php
@@ -26,6 +26,15 @@ $allowed_html = array(
 		'href' => array(),
 	),
 );
+$user = wp_get_current_user();
+$name = $user->first_name;
+$surname = $user->last_name;
+$current_avatar = get_avatar_url($user->ID, ['size' => 300]);
+$email = $user->user_email;
+$name = $user->display_name;
+$birthday = get_user_meta($user->ID, 'birthday', true);
+$gender = get_user_meta($user->ID, 'gender', true);
+$phone = get_user_meta($user->ID, 'billing_phone', true);
 ?>
 
 <?php
@@ -37,12 +46,34 @@ $allowed_html = array(
 	do_action( 'woocommerce_account_dashboard' );
 ?>
 <div class="row">
-	<div class="col-xl-4 col-lg-12">
-		<?php do_action('woocommerce_account_edit-account_endpoint'); ?>
+	<div class="col-xl-3 col-lg-12 profile-container d-flex align-items-center flex-column">
+        <h2 class="pt-5 custom-bold"><?php esc_html_e('Profile', 'woocommerce'); ?></h2>
+        <img src="<?= $current_avatar?>" alt="profile image" class="profile-image">
+        <div class="custom-bold my-3">
+            <span><?= $name?> <?= $surname?></span>
+        </div>
+        <div class="custom-bold mb-3">
+            <span><?= $email?></span>
+        </div>
+        <div class="special-info mb-4">
+            <div>
+                <span><?php esc_html_e('Birthday', 'woocommerce'); ?>  <span class="custom-bold"><?= $birthday?></span></span>
+            </div>
+            <div>
+                <span><?php esc_html_e('Gender', 'woocommerce'); ?>  <span class="custom-bold"><?= $gender?></span></span>
+            </div>
+            <div>
+                <span><?php esc_html_e('Phone', 'woocommerce'); ?>  <span class="custom-bold"><?= $phone?></span></span>
+            </div>
+        </div>
+        <a class="btn btn-edit" href="<?php echo esc_url( wc_get_account_endpoint_url('edit-account') );?>"><i class="esl-edit esl-reg-1"></i>   <?php esc_html_e('Edit', 'woocommerce'); ?></a>
 	</div>
-	<div class="col-xl-8">
+	<div class="col-xl-9 dash-orders mt-xl-0 mt-4">
 		<?php do_action('woocommerce_account_orders_endpoint'); ?>
 	</div>
+    <div class="dash-wishlist mt-4 col-xl-12">
+        <?php do_action('woocommerce_account_webtoffee-wishlist_endpoint'); ?>
+    </div>
 </div>
 <?php
 	/**
